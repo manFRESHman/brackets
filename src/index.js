dc1267ae5baf4b22762221ba7d9124b1729a5b0a
Igor Tolobaev <igortolobaev@gmail.com>
2020-10-05 19:02:26 +0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let ans = true;
+  let open = bracketsConfig.slice(0).map(e => e[0]);
+  let close = bracketsConfig.slice(0).map(e => e[1]);
+  bracketsConfig.forEach(e => {
+    let opened = [];
+    str.split('').forEach(c => {
+      if(close.some(elem => elem === c)){
+        if(open.indexOf(c) === close.indexOf(c) && (!opened.length || opened[opened.length - 1] !== c)){
+          opened.push(c);
+        }
+        else if(!opened.length) ans = false;
+        else if(open.indexOf(opened[opened.length - 1]) === close.indexOf(c)) opened.pop();
+        else ans = false;
+      }
+      else if(open.some(elem => elem === c)) opened.push(c);
+    });
+    if(opened.length) ans = false;
+  });
+  return ans;
 }
